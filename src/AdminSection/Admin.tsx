f0a87ad0a9e86678b72bdd3ca305c721a3ae0057
LipinVV <vit.lipin@gmail.com>
2021-08-13 08:58:49 +0300

    changes file system

--- src/AdminSection/Admin.tsx
+++ src/AdminSection/Admin.tsx
@@ -8,15 +8,19 @@ export const Admin = ({accessFn, state}: any) => {
 
     return (
         <div className='admin'>Admin
-            <Access accessFn={accessFn} state={state}/>
-            <br/>
-            {!state && <Registration/>}
-            <div className='admin__data-updater'>Check the whole data
-                <button
-                    type='button'
-                    onClick={statusOfPersonalInfo}>Check nicknames
-                </button>
-            </div>
+
         </div>
     )
 }
+
+
+// in case of needed soon, or delete after a week
+// <Access accessFn={accessFn} state={state}/>
+// <br/>
+// {!state && <Registration/>}
+// <div className='admin__data-updater'>Check the whole data
+//     <button
+//         type='button'
+//         onClick={statusOfPersonalInfo}>Check nicknames
+//     </button>
+// </div>
